352e1d4975379950e378ff20cd7ae90f5d9281d6
@mirfan19 <m.irfanmaulana99@gmail.com>
2024-11-17 13:57:52 +0700

    Review Authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-// const { campgroundSchema, reviewSchema } = require('../schemas.js');
-// const ExpressError = require('../utils/ExpressError');
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware.js');
 const Campground = require('../models/campground');
 
-
-
- 
 
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -21,7 +16,6 @@ router.get('/new', isLoggedIn, (req, res) =>{
 
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     campground.author = req.user._id;
     await campground.save();
@@ -30,11 +24,16 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 }));
 
 router.get('/:id', catchAsync(async (req, res,) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
     console.log(campground);
-    if(!campground){
-        req.flash('error', 'Cannot find that campground!')
-        return res.redirect('/campgrounds')
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
     }
     res.render('campgrounds/show', { campground });
 }));
