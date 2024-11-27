334c349ae9468fbb22ba4acfd61d2d99eae82d5a
@mirfan19 <m.irfanmaulana99@gmail.com>
2024-11-27 18:16:46 +0700

    Restructure Routes

--- routes/users.js
+++ routes/users.js
@@ -6,28 +6,17 @@ const users = require('../controllers/users')
 const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync (users.register));
 
-router.post('/register', catchAsync (users.register));
+router.route('/login')
+    .get(users.renderLogin)
+    .post(
+        storeReturnTo,
+        passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
+        users.login);  
 
-router.get('/login', users.renderLogin)
-
-router.post('/login',
-    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
-    storeReturnTo,
-    // passport.authenticate logs the user in and clears req.session
-    passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
-    // Now we can use res.locals.returnTo to redirect the user after login
-    users.login);
-
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
-    });
-}); 
+router.get('/logout', users.logout); 
 
 module.exports = router;
