493a6c5d580abc3737bc3930effb3e127dcaaa79
@mirfan19 <m.irfanmaulana99@gmail.com>
2024-12-15 07:49:47 +0700

    add cloudinary and cloudniary storage

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ if(process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-console.log(process.env.CLOUDINARY_CLOUD_NAME)
-console.log(process.env.CLOUDINARY_KEY)
-console.log(process.env.CLOUDINARY_SECRET)
+// console.log(process.env.CLOUDINARY_CLOUD_NAME)
+// console.log(process.env.CLOUDINARY_KEY)
+// console.log(process.env.CLOUDINARY_SECRET)
 
 const express = require('express');
 const path = require('path') ;
